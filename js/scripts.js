c1776810ab9bf366d719a560d6dc96ddc6aff135
Michał Sikorski <sikorhurricane@gmail.com>
2018-03-15 16:09:54 +0100

    Start Kanban

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+$(function() {
+
+	function randomString() {
+    	var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ';
+    	var str = '';
+    	for (var i = 0; i < 10; i++) {
+        	str += chars[Math.floor(Math.random() * chars.length)];
+    	}
+    	return str;
+	}
+
+	function Column(name) {
+		var self = this;
+
+		this.id = randomString();
+		this.name = name;
+		this.$element = createColumn();
+
+		function createColumn() {
+			var $column = $('<div>').addClass('column');
+			var $columnTitle = $('<h2>').addClass('column-title').text(self.name);
+			var $columnCardList = $('<ul>').addClass('column-card-list');
+			var $columnDelete = $('<button>').addClass('btn-delete').text('x');
+			var $columnAddCard = $('<button>').addClass('add-card').text('Add a card');
+		}
+
+
+	}
+
+});
